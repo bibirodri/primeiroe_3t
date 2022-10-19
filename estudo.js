7640c0ab5f98ebe5308aa9ef67e4c7727df7a951
bibirodri <114074984+bibirodri@users.noreply.github.com>
2022-10-19 09:09:39 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -40,3 +40,10 @@ function calcula(){
     document.write("resultado: "+res);
 } 
  
+function somaNota(){
+    let n1 = document.getElementById("t1").Value;
+    let n2 = document.getElementById("t2").Value;
+    let n3 = document.getElementById("t3").Value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resutado").innerHTML = "soma: " + r;
+}
